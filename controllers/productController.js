a898afd854f1398229f9b0a2f121b9c915956dfb
Angel Maria Das <angelmariadas@gmail.com>
2022-11-25 12:20:48 +0530

    listProducts - API controller update - integrate with mongoDB

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,52 +1,13 @@
 
-var products = [
-    {
-        "pId":1,
-        "name": 'Milk',
-        "price":25,       
-        "currency":"rupees",
-        "category":"Diary",
-        "remainingCount":15
-    },
-    {
-        "pId":2,
-        "name": 'Carrot',
-        "price":100,        
-        "currency":"rupees",
-        "category":"Vegtables",
-        "remainingCount":0
-    },
-    {
-        "pId":3,
-        "name": 'Apple',
-        "price":120,
-        "currency":"rupees",
-        "category":"Fruits",
-        "remainingCount":10
-    },
-    {
-        "pId":4,
-        "name": 'Oats',
-        "price":50,
-        "currency":"rupees",
-        "category":"Grain",
-        "remainingCount":5
-    },
-    {
-        "pId":5,
-        "name": 'Orange',
-        "price":90,
-        "currency":"rupees",
-        "category":"Fruit",
-        "remainingCount":8
-    }
-
-]
+var Container = require("typedi").Container;
 module.exports.listProducts = async()=>{
 
     return await new Promise((resolve,reject)=>{
-        //Could use db query in real scenario
-        let list = products.filter((item)=>{if(item.remainingCount>0)return item})
-        resolve({"statusCode":200,"response":list})
+
+        var db = Container.get('mongoConnection').db('OnlinePurchase')
+        db.collection('products').aggregate([{$match:{"remainingCount":{$gt:0}}},{$project:{"_id":0}}]).toArray()
+        .then((list)=>{
+            resolve({"statusCode":200,"response":list})
+        })
     })
 }
