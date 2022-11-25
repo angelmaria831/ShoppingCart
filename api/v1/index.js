d76d11b5f79676d6374c46e22ab8a078846c70ac
Angel Maria Das <angelmariadas@gmail.com>
2022-11-25 12:19:50 +0530

    Add userRoutes for cart related APIs

--- api/v1/index.js
+++ api/v1/index.js
@@ -4,9 +4,10 @@
 
 const express = require('express'),
       router = express.Router(),
-      productRoutes = require('../v1/productRoutes');
-
+      productRoutes = require('../v1/productRoutes'),
+      userRoutes = require('../v1/userRoutes');
 
 router.use(productRoutes);
+router.use(userRoutes)
 
 module.exports = router;
---
+++ api/v1/userRoutes.js
@@ -0,0 +1,41 @@
+/**
+ * 
+ * Routes to define user cart related APIs
+ * 
+ */
+
+ const express = require('express'),
+ router = express.Router(),
+ userController = require('../../controllers/userController');
+
+ /**
+ * 
+ * Name : /api/v1/updateCart/:operation/:pId
+ * Definition : API to add or remove item to cart
+ * Input (params):operation = "add/remove",pId = productId to be added
+ * Output : String message of success or failure
+ * 
+ */
+
+router.patch('/updateCart/:operation/:pId',async(req,res)=>{
+
+    let userId = req.headers.userid;
+    let reqDTO = req.params;
+
+    if(reqDTO.operation == "add"){
+        await userController.addItemToCart(userId,reqDTO).then((apiresponse)=>{
+            res.status(apiresponse.statusCode)
+                .send(apiresponse.response)
+         })
+    }else if(reqDTO.operation == "remove"){
+        await userController.removeItemFromCart(userId,reqDTO).then((apiresponse)=>{
+            res.status(apiresponse.statusCode)
+                .send(apiresponse.response)
+         })
+    }else res.status().send("Invalid payload")
+
+})
+
+
+
+module.exports = router;
